08464400f5a0d5ea49cf18bd6389e5a9b3a830ef
philip <philip@firesoft.gr>
2018-04-03 11:51:05 +0200

    Code Cleanup

--- src/main/scala/firephil/codegolf/crazy8s/Main.scala
+++ src/main/scala/firephil/codegolf/crazy8s/Main.scala
@@ -4,9 +4,35 @@ import scala.collection.mutable.ListBuffer
 import scala.util.Random
 
 object Main extends App{
-  //
+
+  def randomize8(a:Int, b:Int) : ListBuffer[String]={
+    var characters: IndexedSeq[Int] =(33 to 47)++(58 to 126)
+    //shuffle the character set
+    characters = Random.shuffle(characters)
+
+    var result = ListBuffer[String]()
+    var k = 0
+    (a to b).toList.foreach{e=>{
+          if(k == characters.length){
+            k=0
+            characters = Random.shuffle(characters)
+          }
+          if (e % 8 == 0) {
+            result += characters(k).toChar.toString
+            k += 1
+          }
+          else{
+            result += e.toString
+            k+=1
+            println(k)
+        }
+      }
+    }
+    result
+  }
+  //golfed version
   def r(a:Int, b:Int)={
-    var l=(33 to 47).toList:::(58 to 126).toList
+    var l=(33 to 47)++(58 to 126)
     l=Random.shuffle(l)
     var x=ListBuffer[String]()
     var k=0
@@ -18,6 +44,5 @@ object Main extends App{
       else{x+=e.toString
         k+=1}}}
     x}
-
-  print(r(10,40))
+  print(randomize8(10,1000))
 }
