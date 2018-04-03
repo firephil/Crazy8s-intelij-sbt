807f5519017a268f7dd278fbea89d0ddea52a611
philip <philip@firesoft.gr>
2018-04-03 14:36:56 +0200

    Added functional style solution

--- src/main/scala/firephil/codegolf/crazy8s/Main.scala
+++ src/main/scala/firephil/codegolf/crazy8s/Main.scala
@@ -3,32 +3,42 @@ package firephil.codegolf.crazy8s
 import scala.collection.mutable.ListBuffer
 import scala.util.Random
 
-object Main extends App{
+object Main extends App {
 
-  def randomize8(a:Int, b:Int) : ListBuffer[String]={
-    var characters: IndexedSeq[Int] =(33 to 47)++(58 to 126)
+  def randomize8(a: Int, b: Int): ListBuffer[String] = {
+    var characters: IndexedSeq[Int] = (33 to 47) ++ (58 to 126)
     //shuffle the character set
     characters = Random.shuffle(characters)
 
     var result = ListBuffer[String]()
     var k = 0
-    (a to b).toList.foreach{e=>{
-          if(k == characters.length){
-            k=0
-            characters = Random.shuffle(characters)
-          }
-          if (e % 8 == 0) {
-            result += characters(k).toChar.toString
-            k += 1
-          }
-          else{
-            result += e.toString
-            k+=1
-            println(k)
-        }
+    (a to b).toList.foreach { e => {
+      if (k == characters.length) {
+        k = 0
+        characters = Random.shuffle(characters)
+      }
+      if (e % 8 == 0) {
+        result += characters(k).toChar.toString
+        k += 1
+      }
+      else {
+        result += e.toString
+        k += 1
+        println(k)
       }
     }
+    }
     result
+  }
+
+  def randomizeStream(a: Int, b: Int) = {
+    val chars = (33 to 47) ++ (58 to 126)
+
+    var stream :Stream[String] = (a to b).toStream.map {
+      case x@multiple if x % 8 == 0 => chars(Random.nextInt(chars.length)).toChar.toString
+      case x => String.valueOf(x)
+    }
+    stream
   }
   //golfed version
   def r(a:Int, b:Int)={
@@ -44,5 +54,12 @@ object Main extends App{
       else{x+=e.toString
         k+=1}}}
     x}
-  print(randomize8(10,1000))
+  def S(a: Int, b: Int)={
+    val c=(33 to 47)++(58 to 126)
+    val r = (a to b).toStream.map {case x if x % 8 == 0=>c(Random.nextInt(c.length)).toChar.toString
+      case x => String.valueOf(x)}
+    r}
+
+
+  println(S(10,20).toList)
 }
